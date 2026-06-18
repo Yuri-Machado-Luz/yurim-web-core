a4369e569ab8b711c9c833fcbed2801ae55fb50c
Yuri Machado Luz <yurimachadoluz@gmail.com>
2026-06-18 20:29:48 -0300

    feat: remoção de assets de public folder

--- apps/blog/public/assets/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as DarkLogo } from "./dark-favicon.png";
-export { default as Logo } from "./favicon.svg";
-export { default as ProfilePicture } from "./profile-picture.png";
-export { default as LogoText } from "./text-logo-light.svg";
